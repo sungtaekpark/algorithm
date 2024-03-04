c35307a5c1bc3820c54d155a5573230041e9d338
sungtaek <76993672+sungtaekpark@users.noreply.github.com>
2024-03-04 17:54:34 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+function solution(a, b, c) {
+    // var answer = 0; 
+    // 1. 모두 다른 경우
+    // 2. 두숫자가 같은 경우
+    // 3. 모두 같은 경우
+    
+    if(a !== b && a !== c && b !== c) return a + b + c;
+    else if(a === b && b === c) return (a+b+c)*(a*a+b*b+c*c)*((a*a*a)+ (b*b*b) + (c*c*c))
+    else return (a+b+c)*(a*a+b*b+c*c);
+ 
+    
+//     return answer;
+}
