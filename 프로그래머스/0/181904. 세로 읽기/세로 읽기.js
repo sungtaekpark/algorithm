8f2d7f492d9fde98aef9e58f9f84a76fae23eb0a
sungtaek <76993672+sungtaekpark@users.noreply.github.com>
2024-03-18 18:43:17 +0900

    [level 0] Title: 세로 읽기, Time: 3.75 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,10 @@
+function solution(my_string, m, c) {
+    let answer = "";
+    
+    for(i=c-1; i<my_string.length; i+=m) {
+        console.log(i)
+        answer += my_string[i]
+    }
+
+    return answer;
+}
