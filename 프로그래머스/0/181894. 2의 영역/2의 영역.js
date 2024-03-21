f9d3849f7c55317d8b3115bed13b5daa083ac0f3
sungtaek <76993672+sungtaekpark@users.noreply.github.com>
2024-03-21 23:05:02 +0900

    [level 0] Title: 2의 영역, Time: 2.77 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -3,13 +3,8 @@ function solution(arr) {
     arr.map((a, index) => {
         a === 2 && target.push(index);
     })
-    
-    // return target.length > 1 ? arr.slice(target[0], target.length) :
-    // target.length === 1 ? arr[target[0]] :
-    // -1
-    
-    console.log(target)
-    return target.length > 2 ? arr.slice(target[0], target[target.length-1] +1)
+
+    return target.length > 2 ? arr.slice(target[0], target[target.length-1]+1)
     : target.length === 2 ? arr.slice(target[0], target[1]+1)
     : target.length === 1 ? [arr[target[0]]]
     : [-1];
