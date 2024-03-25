30361ef4b4df7b0034673e7fda7f6c51509f741c
sungtaek <76993672+sungtaekpark@users.noreply.github.com>
2024-03-26 00:27:49 +0900

    [level 0] Title: 1로 만들기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,19 @@
+function solution(num_list) {
+    let count = 0;
+    for (num of num_list) {
+        let number = num;
+        while(true) { // 반복
+            if(number === 1) { 
+                break;
+            };
+            if(number%2 === 0) {
+                number=number/2;
+                count+=1;
+            } else {
+                number=(number-1)/2;
+                count+=1;
+            }
+        }
+    }
+    return count;
+}
